c8fab2ee03fddfab2341e9c2ea88a6dfe51acbec
Nelusa <nela.letochova@czechitas.cz>
2023-07-19 09:45:49 +0200

    Figma design added

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,12 +1,12 @@
-import { Button } from './Button';
+import { Button } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 };
 
@@ -14,26 +14,32 @@ export default {
 export const Primary = {
   args: {
     primary: true,
-    label: 'Button',
+    label: "Button",
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/lwoCTpbKoJ5JBbuBtKVKOZ/Storybook-Connect-Guide-(Community)?type=design&node-id=33014-5312&mode=dev",
+    },
   },
 };
 
 export const Secondary = {
   args: {
-    label: 'Button',
+    label: "Button new",
   },
 };
 
 export const Large = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: "large",
+    label: "Button",
   },
 };
 
 export const Small = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: "small",
+    label: "Button",
   },
 };
--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,20 +1,26 @@
-import { Header } from './Header';
+import { Header } from "./Header";
 
 export default {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 };
 
 export const LoggedIn = {
   args: {
     user: {
-      name: 'Jane Doe',
+      name: "Jane Doe",
+    },
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/lwoCTpbKoJ5JBbuBtKVKOZ/Storybook-Connect-Guide-(Community)?type=design&node-id=53-9673&mode=dev",
     },
   },
 };
